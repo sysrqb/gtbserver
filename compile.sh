d5a0665a54714e9af6993da93ad97e074b7d5199
FM <matthew.finkel@gmail.com>
2011-07-25 16:47:42 -0400

    also added -ggdb option into compile line

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang -Wl,-L/usr/lib/mysql,-lmysqlclient comm.c gtbserver.c mysqlconn.c `pkg-config gnutls --cflags --libs`
+clang -Wl,-L/usr/lib/mysql,-lmysqlclient -ggdb comm.c gtbserver.c mysqlconn.c `pkg-config gnutls --cflags --libs`
--- mysqlconn.h
+++ mysqlconn.h
@@ -1,4 +1,4 @@
-#include <mysql/mysql.h>
+#include <mysql.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
