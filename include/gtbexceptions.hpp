fbd889add5dfc1c0712797344383173fb4eec055
FM <matthew.finkel@gmail.com>
2012-03-26 20:31:34 -0400

    Added gtbexceptions class
    Contains PatronException

---
+++ include/gtbexceptions.hpp
@@ -0,0 +1,30 @@
+/*
+ * GUARD the Bridge
+ * Copyright (C) 2012  Matthew Finkel <Matthew.Finkel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <exception>
+
+class PatronException : public std::exception
+{
+  public:
+    PatronException(std::string m="Patron Exception caught!") : msg(m) {}
+    ~PatronException() throw() {}
+    const char* what() const throw() { return msg.c_str(); }
+
+  private:
+    std::string msg;
+};
