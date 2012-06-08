b11eefe68d182972a2e7c569ccb29426595018a7
FM <matthew.finkel@gmail.com>
2012-06-07 23:45:48 -0400

    Commented all methods for use by Doxygen

--- include/gtbcommunication.hpp
+++ include/gtbcommunication.hpp
@@ -59,17 +59,36 @@
 #define AUTHSIZE 60
 
 
+/**
+ * Class: GTBCommunication
+ * 
+ * \brief Handles communication with network clients
+ *
+ * Facilitates establishing a connection using TLS with client,
+ * as well as sending, retrieving, and updating patron information
+ * in the selected database
+ */
 class GTBCommunication {
     gnutls_priority_t * m_pPriorityCache;
     gnutls_certificate_credentials_t * m_pX509Cred;
     gnutls_dh_params_t * m_pDHParams;
     gnutls_session_t m_aSession;
+    /** Stores IP address of incoming client */
     char m_vIPAddr[INET6_ADDRSTRLEN];
     std::string m_sHash;
+    /** SQL connection handler */
     MySQLConn * m_MySQLConn;
+    /** Debug bit mask */
     int debug;
 
   public:
+    /** \brief Constructor with optional debug output 
+     * 
+     * Constructor defaults to zero debug output.
+     * For additional debugging information, increase the value
+     * at instantiation.
+     * \sa debug
+     */
     GTBCommunication(int indebug = 0)
     {
       debug = indebug;
@@ -78,8 +97,11 @@ class GTBCommunication {
       m_pX509Cred = (gnutls_certificate_credentials_t * ) 
           operator new (sizeof (gnutls_certificate_credentials_t));
       m_MySQLConn = new MySQLConn();
-      m_pDHParams = (gnutls_dh_params_t *) operator new (sizeof (gnutls_dh_params_t));
+      m_pDHParams = (gnutls_dh_params_t *) operator 
+          new (sizeof (gnutls_dh_params_t));
     }
+
+    /** \brief Deconstructor to clean up dynamic info */
     ~GTBCommunication()
     {
       delete m_pDHParams;
@@ -88,43 +110,197 @@ class GTBCommunication {
       delete m_pPriorityCache;
     }
 
-    /*GNUTLS related methods*/
-    void initGNUTLS(); 
-    void deinitGNUTLS(); 
+    /* GNUTLS related methods */
+    /** \brief Handle initialization of GNUTLS backend
+     *
+     * Directly calls gnutls_global_init and subsequently calls for
+     * the loading of Certificate files and setting priority values
+     */
+    void initGNUTLS();
+
+    /** \brief Handle the deinitialization of GNUTLS
+     *
+     * Calls gnutls_deinit on the current TLS session
+     */
+    void deinitGNUTLS();
+
+    /** \brief Prepare a new TLS Session
+     * 
+     * Initialize a new session for the current client.
+     * Set certificate to use and connection requirements.
+     */
     void initTLSSession ();
+ 
+    /** \brief Prepare Diffie-Hellman Parameters
+     *
+     * Initialize Diffie-Hellman parameters for new TLS session
+     * and generate new keys.
+     */
     int generateDHParams ();
+
+    /** \brief Loads server-side certificates and keys */
     void loadCertFiles ();
 
     /*Misc mutator and accessor methods*/
+
     void getClientInfo (int i_sockfd);
+
+    /** \brief Bind to open port
+     *
+     * Find free port and bind it it.
+     */
     int getSocket ();
+
+    /** \brief Return incoming client as IPv4 or IPv6
+     *
+     * Checks if client is using IPv4 or IPv6 and sets/initializes 
+     * the structs accordingly. Depending on the protocol that is 
+     * being used, i_sa will either be struct in_addr or in6_addr.
+     * sin_addr or sin6_addr, respectively, is then the return value.
+     *
+     * \param i_sa struct sockaddr_storage; Address of peer layer
+     * as known to the communication layer
+     */
     void *getInAddr (struct sockaddr *i_sa);
+
+    /** \brief Accessor: Returns the IP Address of the current client */
     std::string getClientAddr(){ return std::string(m_vIPAddr); } 
 
     /*GNUTLS variables accessor methods */
+
+    /** \brief Accessor: Returns current priorities set in TLS session */
     gnutls_priority_t * getPriorityCache() { return m_pPriorityCache; }
+
+    /** \brief Accessor: Returns struct holding certificate info */
     gnutls_certificate_credentials_t * getCertCred() { return m_pX509Cred; }
+
+    /** \brief Accessor: Return Diffie-Hellman parameters */
     gnutls_dh_params_t * getDHParams() { return m_pDHParams; }
+
+    /** \brief Accessor: Return current session info */
     gnutls_session_t * getSession() { return &m_aSession; }
 
     /*Communication with client*/
+
+    /** \brief Return errorcode to client. */
     int sendFailureResponse(int errorcode);
+
+    /** \brief Return 0 = Successful to client. */
     int sendAOK();
+
+    /** \brief Send only sizeof(int) bytes containing i_nRetVal 
+     * to client. 
+     */
     int sendNopes(int i_nRetVal);
+
+    /** \brief Return number of running vans to client.
+     *
+     * Checks that i_aPBReq is a valid request. If so, the database 
+     * is queried and the result is returned in a Response packet.
+     */
     int sendNumberOfCars(Request * i_aPBReq);
+
+    /** \brief Unimplemented: May be removed */
     int moveKey();
+
+    /** \brief Request to authenticate client.
+     *
+     * Checks that i_aPBReq is a valid request. If so, the provided
+     * values in the packet are verified.
+     *
+     * \todo Store car number, ip address, cert uid for future 
+     * authentication
+     */
     int authRequest (Request * i_aPBReq);
+
+    /** \brief Request for current patrons assigned to client.
+     *
+     * Checks that i_aPBReq is a valid request. If so, the current
+     * patrons are returned to the client, excluding those that have
+     * have not be modified and the client already has in its DB.
+     *
+     * \todo Verify car number, ip address, cert uid
+     */
     int currRequest (Request * i_aPBReq, Response * i_apbRes);
+
+    /** \brief Request to update a patron's info.
+     *
+     * Checks that i_aPBReq is a valid request. If so, the patrons
+     * information is updated in the database.
+     *
+     * \todo Authenticate client based on car number, ip address, cert uid
+     * \todo Conflict resolution if conflicting modified field exist
+     */
     int updtRequest (Request * i_aPBReq, Response * i_apbRes);
+
+    /** \brief Handle incoming request.
+     *
+     * When a request is received, determine the request ID number. Using
+     * this number, the request is then handed off to the correct method
+     *
+     * \sa authRequest
+     * \sa currRequest
+     * \sa updtRequest
+     */
     int dealWithReq (Request i_sPBReq);
+
+    /** \brief Perform TLS handshake with client
+     *
+     * After a client connects to the server, perform TLS handshake
+     * with client to establish TLS session. Once complete, verify 
+     * the client provides a certificate and that it was signed by
+     * our CA
+     *
+     * \todo Figure out why certificate verification segfaults =(
+     *
+     * \param fdAccepted File descriptor for opened socket connection
+     * \param sockfd File descriptor for bound socket
+     */
     int handleConnection (int, int);
+
+    /** \brief Listen for a client connection
+     *
+     * Initialize TLS session. When a client connects, store the client's
+     * IP address.
+     *
+     * \param i_fdSock File descriptor for bound socket
+     * \return File descriptor for opened socket connection
+     */
     int listeningForClient (int i_sockfd);
+
+    /** \brief Retrieves all incoming requests from client
+     *
+     * Once the connection is established, all request/incoming packets
+     * are received and returned to plaintext.
+     *
+     * param aPBReq Where incoming request is stored because it's content
+     * will be used throughout the request's lifetime
+     */
     void receiveRequest(Request *);
 
   private:
+    /** \brief Sends response to client.
+     *
+     * Once a request has been completed the response is sent to the client.
+     *
+     * If all input parameters are NULL and i_nRetVal is NOT 0, then return an
+     * error message
+     * If all input parameters are NULL and i_nRetVal is 0, then return
+     * a successful reply
+     * If at least the i_pbRes parameter is NOT NULL, then send the response
+     * packet.
+     * If at least one parameter is NOT NULL, but i_pbRes is NULL then do not
+     * send a response.
+     *
+     * \param i_nRetVal The return value from the request
+     * \param i_pbReq The request received from the client
+     * \param i_pbRes The response created to be send to the client
+     * \param i_pbPL The patron[s] to be sent to the client if created
+     * as a result of the request
+     */
     int sendResponse(int i_nRetVal, 
             Request * i_pbReq, 
 	    Response * i_pbRes, 
 	    PatronList * i_pbPL);
 };
-#endif
+#endif  // gtbcommunication_h
